331513f72d4b052c4cf60b7d57b14b02982c1ca1
Sean Kenny <sean.kenny@hotmail.com>
2016-03-16 15:44:46 +0000

    Add credentials object
    This encapsulates the url and token for github.com and any github
    enterprise installs along the way.

---
+++ lib/alfred-github/github/credentials.rb
@@ -0,0 +1,22 @@
+module AlfredGithub
+  class Github
+    class Credentials
+
+      attr_reader :credentials_hash
+      private :credentials_hash
+
+      def initialize(credentials_hash)
+        @credentials_hash = credentials_hash
+      end
+
+      def base_url
+        credentials_hash.fetch('url')
+      end
+
+      def token
+        credentials_hash.fetch('token')
+      end
+
+    end
+  end
+end
