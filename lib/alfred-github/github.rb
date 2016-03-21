80b7f4aa85f8eb3e36cf8466b4df5988d55b4b34
Sean Kenny <sean.kenny@hotmail.com>
2016-03-21 16:35:46 +0000

    Add outline for rebuilding repositories

---
+++ lib/alfred-github/github.rb
@@ -0,0 +1,13 @@
+module AlfredGithub
+  class Github
+
+    def self.rebuild_repositories
+      new.rebuild_repositories
+    end
+
+    def rebuild_repositories
+
+    end
+
+  end
+end
---
+++ spec/lib/alfred-github/github_spec.rb
@@ -0,0 +1,7 @@
+module AlfredGithub
+  RSpec.describe Github do
+    describe '.rebuild_repositories' do
+
+    end
+  end
+end
